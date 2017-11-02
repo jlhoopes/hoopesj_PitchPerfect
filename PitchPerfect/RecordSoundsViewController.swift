0dd23ab1650450dbea8bc54f6fc9f5adec2a3720
Jason Hoopes <JLH@jasons-imac.local>
2017-11-01 21:47:58 -0400

    Lesson 5.8

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -65,8 +65,8 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         }
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if seque.identifier == "stopRecording" {
-            let playSoundsVC = seque.destination as! PlaySoundsViewController
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
             let recordedAudioURL = sender as! URL
             playSoundsVC.recordedAudioURL = recordedAudioURL
         }
